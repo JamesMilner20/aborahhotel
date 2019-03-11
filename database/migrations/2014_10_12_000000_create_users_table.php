8b41c84b9b85fa8fd91150304e20bbe7aed522a4
jamesmilner20 <jamesduah20@gmail.com>
2019-03-11 09:26:48 +0000

    initial user role and password table migrated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('role_id')->index()->unsigned();
+            $table->integer('is_active')->default(0);
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('mobile_number')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
